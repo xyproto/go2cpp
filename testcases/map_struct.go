41c8c99c82b193ae10c8347970577efc99892dc2
Alexander F. Rødseth <52813+xyproto@users.noreply.github.com>
2020-10-17 00:31:24 +0200

    Update map_struct.go

--- testcases/map_struct.go
+++ testcases/map_struct.go
@@ -12,8 +12,8 @@ type Creature struct {
 
 func main() {
 	o := map[int]*Creature{
-		0: {1.2, 3.4, "Bob"},
-		1: {4.5, 6.7, "Alice"},
+		0: &Creature{1.2, 3.4, "Bob"},
+		1: &Creature{4.5, 6.7, "Alice"},
 	}
 	fmt.Println(o[1])
 }
